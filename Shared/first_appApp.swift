7f0d5491dd2e542fce6af8eb33e861009d5a346e
IUGAI <yugaialeksandr@kbu.ac.kr>
2022-10-21 00:39:15 +0900

    first commit

---
+++ Shared/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  Shared
+//
+//  Created by Iugai Aleksandr on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
